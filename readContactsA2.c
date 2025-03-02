fa2c528a94b0618d2d69c3a83f477d22bed7df8f
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2025-03-02 15:46:24 -0500

    Update readContactsA2.c

--- readContactsA2.c
+++ readContactsA2.c
@@ -10,11 +10,11 @@ int readContacts (char * fileName){
         return -1;  // Exit with an error code
     }
 
-    // determine end of file
-    long endOfFile;
-    fseek(file, 0, SEEK_END);  // Move to the end
-    endOfFile = ftell(file); // Store the end position
-    printf("\nEnd of File Position: %ld\n", endOfFile);
+    // // determine end of file
+    // long endOfFile;
+    // fseek(file, 0, SEEK_END);  // Move to the end
+    // endOfFile = ftell(file); // Store the end position
+    // printf("\nEnd of File Position: %ld\n", endOfFile);
 
     // Move to the beginning of the file
     fseek(file, 0, SEEK_SET);
@@ -27,26 +27,39 @@ int readContacts (char * fileName){
 
         printf("\n\n************************************************************\n\n");
 
-
+        // process employee ID
         fseek(file, retrievedContact.empIdPosn, SEEK_SET);
         int employeeIDRead;
         fread(&employeeIDRead, sizeof(int), 1, file);
+        printf("Employee ID: %d\n", employeeIDRead);
 
-        printf("Employee ID:%d\n", employeeIDRead);
-        // printf("First Name:%ld\n", retrievedContact.firstNamePosn);
-        // printf("Last Name: %ld\n", retrievedContact.lastNamePosn);
-        // printf("Email: %ld\n", retrievedContact.emailPosn);
-        // printf("NEXT: %ld\n", retrievedContact.next);
+        // process first name
+        fseek(file, retrievedContact.firstNamePosn, SEEK_SET);
+        int differenceF = retrievedContact.lastNamePosn - retrievedContact.firstNamePosn;
+        char firstName[differenceF];
+        fread(&firstName, sizeof(char), differenceF, file);
+        printf("First Name: %s\n", firstName);
 
+        // process last name
+        fseek(file, retrievedContact.lastNamePosn, SEEK_SET);
+        int differenceL = retrievedContact.emailPosn - retrievedContact.lastNamePosn;
+        char lastName[differenceL];
+        fread(&lastName, sizeof(char), differenceL, file);
+        printf("Last Name: %s\n", lastName);
+
+        // process email
+        fseek(file, retrievedContact.emailPosn, SEEK_SET);
+        int differenceE = retrievedContact.next - retrievedContact.emailPosn;
+        char emailStr[differenceE];
+        fread(&emailStr, sizeof(char), differenceE, file);
+        printf("Email: %s\n", emailStr);
+        
+        printf("NEXT: %ld\n", retrievedContact.next);
 
 
         printf("\n************************************************************\n\n");
 
-
-
     }
-
-
 
     fclose(file);
 
